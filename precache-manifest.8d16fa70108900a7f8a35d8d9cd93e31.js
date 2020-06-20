76dd39447626372787c73911081a60fc40ee1e1e
Shubham Agarwal <shubhamagarwal1694@gmail.com>
2020-06-20 18:22:50 +0000

    deploying

---
+++ precache-manifest.8d16fa70108900a7f8a35d8d9cd93e31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f045c8c3dff375b683229b6d6c6b4549",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fd4b6179882c34ffda2",
+    "url": "/static/css/main.c0483124.chunk.css"
+  },
+  {
+    "revision": "cd91a4d852fab522e42b",
+    "url": "/static/js/2.2c0b390f.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.2c0b390f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd4b6179882c34ffda2",
+    "url": "/static/js/main.a542fe00.chunk.js"
+  },
+  {
+    "revision": "6cd2ea1056b03af6b356",
+    "url": "/static/js/runtime-main.538a3dd4.js"
+  },
+  {
+    "revision": "71926df275f10a9063d8879e32ef8727",
+    "url": "/static/media/Logo_SA.71926df2.png"
+  },
+  {
+    "revision": "c20f3c669eb30dbc3abd73260bb2ffc8",
+    "url": "/static/media/avatar.c20f3c66.jpg"
+  }
+]);
